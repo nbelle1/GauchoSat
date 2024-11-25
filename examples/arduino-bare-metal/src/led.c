b73b5b03cc5d91a8438520ec0ebdaa98d781e9a5
Kpazawala Windross <kpazawala@gmail.com>
2024-11-24 16:55:15 -0800

    still some errors that make it hard to get it running will fix and add readme to implement the code on your machine for necessary libs

--- examples/arduino-bare-metal/src/led.c
+++ examples/arduino-bare-metal/src/led.c
@@ -2,14 +2,14 @@
 #include <util/delay.h>
 #include <stdio.h>
 
-#define CPU_ClOCK 16000000UL
+#define CPU_CLOCK 16000000UL
 
 int main() {
-    //LED->BUILTIN : PC7
-    DDRB |= (1 << PC7); // Set PC7 as output
+    // LED -> BUILTIN : PC7
+    DDRC |= (1 << PC7); // Set PC7 as output (on Port C)
 
     while (1) {
-        PORTB ^= (1 << PC7); // Toggle PB0
+        PORTC ^= (1 << PC7); // Toggle PC7 (on Port C)
         _delay_ms(500);
     }
 }
